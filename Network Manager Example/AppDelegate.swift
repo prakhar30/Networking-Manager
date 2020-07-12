ccbcfff0b2bd0dd097554e8602091f08b9111dfc
Prakhar Tripathi <prakhar.tripathi@theredpandas.com>
2020-07-12 15:35:48 +0530

    added new project

---
+++ Network Manager Example/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Network Manager Example
+//
+//  Created by Prakhar Tripathi on 12/07/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Network Manager Example/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Network Manager Example
+//
+//  Created by Prakhar Tripathi on 12/07/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+    }
+}
